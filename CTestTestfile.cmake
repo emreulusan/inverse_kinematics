e56059cd33f734e2973242b846e9ab0840a0f14d
emreulusan <theulusan@gmail.com>
2020-12-29 23:13:36 +0300

    ok!

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/emre/Desktop/Inverse_Kinematics
-# Build directory: /home/emre/Desktop/Inverse_Kinematics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
